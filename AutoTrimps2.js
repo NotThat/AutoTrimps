31cceb18d7d473a1f449b750bd9f55f6eaac8734
NotThat <NotThat>
2018-08-05 23:26:54 +0300

    patch 11

--- AutoTrimps2.js
+++ AutoTrimps2.js
@@ -116,7 +116,10 @@ var startupDelay = 2500;    //How long to wait for everything to load
 setTimeout(delayStart, startupDelay);
 function delayStart() {
     initializeAutoTrimps();
-    if (atscript != null && atscript.basepath == 'https://notthat.github.io/AutoTrimps/')
+    if (atscript != null && atscript.basepath == 'http://localhost:8383/Trimps/Trimps.github.io/AutoTrimps/'){
+        
+    }
+    else
         printChangelog();
     setTimeout(delayStartAgain, startupDelay);
 }
