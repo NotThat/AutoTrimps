0d8df46dee14156273bd8281c935b4a79de72510
NotThat <NotThat>
2018-08-06 01:43:25 +0300

    patch 11

--- AutoTrimps2.js
+++ AutoTrimps2.js
@@ -24,7 +24,7 @@ var atscript = document.getElementById('AutoTrimps-script')
   , modulepath = 'modules/'
   ;
   
-  var ver = 11;
+  var ver = 12;
     
 function delayStart() {
     initializeAutoTrimps();
