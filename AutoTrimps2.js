4a7eaecb947756a85cb6342380a1b8ee2543ddc7
asd <asd@asd-PC.mynet>
2018-09-21 07:59:36 +0300

    no AA without respec. DE only when remaining in zone

--- AutoTrimps2.js
+++ AutoTrimps2.js
@@ -19,7 +19,7 @@ var ATversion = '2.1.7.1'; //when this increases it forces users setting update 
 var local = false;
 //local = true;
 var ver = "22";
-var verDate = "20.9.18";
+var verDate = "21.9.18";
 
 var atscript = document.getElementById('AutoTrimps-script'), 
         basepath = (local ? 'http://localhost:8383/Trimps%204/Trimps.github.io/AutoTrimps/' : 'https://notthat.github.io/AutoTrimps/'),
